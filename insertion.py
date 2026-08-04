062ead7188a36ba77708eceb11a1a14b376e4c67
arshiarb24 <up25g6250077@vistas.ac.in>
2026-08-04 14:39:56 +0530

    Create insertion.py

---
+++ insertion.py
@@ -0,0 +1,25 @@
+class node
+def__init_(self.dataval=none):
+  self.dataval=dataval=dataval
+  self.next=none
+ class slinked list
+  def__init_(self):
+    self.headval=none
+def listprint(self):
+  printval=self.headval
+  while printval is not none:
+    print(print val.dataval)
+    print val=print val.next val
+ def at begining (self.newdata):
+     new node=node(new data)
+new nodenext val=self headval
+self.headval=newnode
+list=slinked list()
+list.headval=node("mon")
+e2=node("tue")
+e3=node("wed")
+list.headval.nextval=e2
+e2.nextval=e3
+list.at beginning("sum")
+list.list print()
+
