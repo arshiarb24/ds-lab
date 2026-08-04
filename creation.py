9d01b2c109639d09df0ebd3b2cb0141bf0c0e6a5
arshiarb24 <up25g6250077@vistas.ac.in>
2026-08-04 14:13:52 +0530

    Create creation.py

---
+++ creation.py
@@ -0,0 +1,45 @@
+class node:
+  def__init_(self,data):
+  self.data=data
+  self.next=none
+class linked list
+   def__init_(self):
+    self.head=none
+def push(self.new_data):
+  new_node=
+  node(new_data)
+  new_node.next=self.head
+  self.head=new_node
+def insert after(self.prev_node,new_data):
+if prev_node is none:
+  print("the given previous node must i linked list".)
+  return
+  new_node=node(new_data)
+  new_node.next=
+  prev_node.next prev_node.next=new node
+def append(self.new_data):
+  new_node=node(new_data)
+  if self head is none:
+    self.head=new_node
+    return
+    last=self.head
+ while(lastnext)
+last=last.next
+last.next=new_node
+  def printlist(self):
+    temp=self.head
+    while(temp):
+      print(temp.data)
+      temp=temp.next
+ if__name_=='_main__':
+llist=linked list()
+llist.append(6)
+llist.push(7)
+llist.push(1)
+llist.append(4)
+llist.insert after(llist.head.next.8)
+print('created linked list is:')
+llis.print list()
+  
+  
+  
